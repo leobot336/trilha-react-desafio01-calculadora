b2fce85284c193bfef5b567a0c8e9c07ca16bfed
leobot336 <114504872+leobot336@users.noreply.github.com>
2024-11-08 15:27:48 -0300

    Update styles.js

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.button`
     padding: 20px;
     border: 1px solid #CDCDCD;
-    background-color: #00AAF0;
+    background-color: #3d3d3d;
     color: #FFFFFF;
     font-size: 24px;
     font-weight: 700;
     flex: 1;
+    border: 0;
     
     &:hover {
         opacity: 0.6;
